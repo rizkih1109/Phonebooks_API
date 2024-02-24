f5b132033246d79656807506191102ac55b834bf
rizkih1109 <rizkihidayatulloh1305@gmail.com>
2024-02-25 00:14:25 +0700

    simple CRUD done

--- routes/users.js
+++ routes/users.js
@@ -3,8 +3,57 @@ var router = express.Router();
 const models = require('../models')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  
+router.get('/', async (req, res, next) => {
+    try {
+        const users = await models.User.findAll()
+        res.json(users)
+    } catch (err) {
+        console.log(err)
+        res.json({ err })
+    }
+});
+
+router.post('/', async (req, res, next) => {
+    try {
+        const { name, phone } = req.body
+        const user = await models.User.create({ name, phone })
+        res.json(user)
+    } catch (err) {
+        console.log(err)
+        res.json({ err })
+    }
+});
+
+router.put('/:id', async (req, res, next) => {
+    try {
+        const { name, phone } = req.body
+        const user = await models.User.update({ name, phone }, {
+            where: {
+                id: req.params.id
+            },
+            returning: true,
+            plain: true
+        });
+        res.json(user[1])
+    } catch (err) {
+        console.log(err)
+        res.json({ err })
+    }
+});
+
+router.delete('/:id', async (req, res, next) => {
+    try {
+        const user = await models.User.findOne({ where: { id: req.params.id } });
+        const userDelete = await models.User.destroy({
+            where: {
+                id: req.params.id
+            }
+        });
+        res.json(user)
+    } catch (err) {
+        console.log(err)
+        res.json({ err })
+    }
 });
 
 module.exports = router;
